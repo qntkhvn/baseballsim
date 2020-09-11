2c042c15740cf3f6b5a2ca08327b177be9ad2bd3
Quang Nguyen <43119108+qntkhvn@users.noreply.github.com>
2020-09-11 13:24:22 -0400

    Update Batter.java

--- src/Batter.java
+++ src/Batter.java
@@ -1,54 +1,40 @@
-
-public class Batter 
-{
+public class Batter {
 	private String name;
-
 	private double average;
 	
 	/**
-	 * Constructor - Creates a batter with the following information: name and average
+	 * Constructor: Creates a batter with the following information: name and average
 	 * 
 	 * @param name    - batter's name
 	 * @param average - batter's average
 	 */
-
-	public Batter(String name, double average) 
-	{
+	public Batter(String name, double average) {
 		this.name = name;
-
 		this.average = average;
 	}
 
-
 	/**
-	 * This function uses batter's average to determine whether they hit the pitch
+	 * Uses batter's average to determine whether they hit the pitch
 	 * or swings and misses (assuming a strike is thrown by pitcher)
 	 * 
 	 * @return true if batter hits; false if batter swings and misses
 	 */
-
-	public boolean hit() 
-	{
-		if (Math.random() <= average) 
-		{
+	public boolean hit() {
+		if (Math.random() <= average) {
 			return true;
 		} 
-		else 
-		{
+		else {
 			return false;
 		}
 
 	}
 	
 	/**
-	 * The get method is used to get the batter's name
+	 * Gets the batter's name
 	 * 
 	 * @return A string representing the batter's name
 	 */
-
-	public String getName() 
-	{
+	public String getName() {
 		return name;
 	}
-
 }
